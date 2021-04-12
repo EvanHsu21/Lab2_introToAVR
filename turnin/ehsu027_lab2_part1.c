e33602ec70fbb79a35fc683702b88d7c637aa115
EvanHsu21 <ehsu027@ucr.edu>
2021-04-11 17:53:23 -0700

    Completed part 1

---
+++ turnin/ehsu027_lab2_part1.c
@@ -0,0 +1,29 @@
+/*	Author: ehsu027
+ *  Partner(s) Name: 
+ *	Lab Section: 22
+ *	Assignment: Lab 2  Exercise 1
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Inse://www.youtube.com/watch?v=bWBlfSRlNC4t DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0x00;
+	DDRB = 0xFF; PORTB = 0x00;
+    /* Insert your solution below */
+    while (1) {
+	if (PINA == 0x01) {
+		PORTB = 0x01;
+	}
+	else {
+		PORTB = 0x00;
+	}
+    }
+    return 1;
+}
